e8567a16e92ed4b049861eb8237ab6033277ac52
Alejandro Almanza <alejandro.almanza@e-vanhost.net>
2021-11-17 10:15:50 -0600

    :children_crossing: checkpoint

--- client2-test.py
+++ client2-test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+from test import bcolors
 
 HOST = "192.168.0.13"
 PORT = 8080
@@ -8,11 +9,13 @@ buffer_size = 8192
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    print("Enviando mensaje...")
+    print("Sending message...")
     s.sendall(b"Im player two")
     sdata = ""
     while not "END_GAME" in sdata:
         print("Waiting for answer...")
         data = s.recv(buffer_size)
         sdata = data.decode()
-        print("Recieved,", sdata, " de", s.getpeername())
+        print(f"Recieved: {bcolors.OKCYAN}{sdata}{bcolors.ENDC}")
+        message = input("Type a message: ")
+        s.sendall(str.encode(message))
