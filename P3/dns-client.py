ae272651ee920762f016fa292b7b23439629acd8
Alejandro Almanza <alejandro.almanza@e-vanhost.net>
2021-12-16 22:27:08 -0600

    :construction: Checkpoint: P3: starting udp protocol

---
+++ P3/dns-client.py
@@ -0,0 +1,16 @@
+import socket
+
+HOST = "127.0.0.1"  # El hostname o IP del servidor
+PORT = 54321  # El puerto usado por el servidor
+msgFromClient = "Hello UDP Server"
+bytesToSend = str.encode(msgFromClient)
+serverAddressPort = ("127.0.0.1", 54321)
+bufferSize = 1024
+
+# Crea un socket UDP del lado del cliente
+
+with socket.socket(socket.AF_INET,socket.SOCK_DGRAM) as UDPClientSocket:
+	# Enviando mensaje al servidor usando el socket UDP
+	UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+	msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+	print("Message from Server {}".format(msgFromServer[0]))
