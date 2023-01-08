7c17ad08aa61ed7e5e6092696ec39b45d01a16cd
Alejandro Almanza <almanza.marfrancisco@hotmail.com>
2023-01-07 22:52:01 -0600

    :construction: Progress: ready for making the file server for project

--- New/projectPrograms/check_last_file.c
+++ New/projectPrograms/check_last_file.c
@@ -13,32 +13,39 @@
 int list_direcory();
 
 int main(int argc, char *argv[]){
-	// struct stat buffer;
-	// int status, fildes;
-	// fildes = open("./Notes.txt", O_RDWR);
-	// status = fstat(fildes, &buffer);
-	// puts("**Printigin in a file**");
-	// printf("Access time  = %s\n", ctime(&buffer.st_mtime));
 	DIR *dp;
 	struct dirent *ep;     
-	short int i = 0;
+	short int i = 0, file_buffer_after = 0, file_buffer_before = 0;
 	system("clear");
 	while(1){
 		printf("[I] Checking directory... %2d\n", i);
-		printf("[I] There are: %d files\n", list_direcory());
+		printf("[I] There are: %d files\n", file_buffer_after = list_direcory());
+		printf("[I] file buffer before: %d\n", file_buffer_after);
+		if(file_buffer_before == file_buffer_after)
+			printf("[I] Not changed! :)\n");
+		else{
+			printf("[I] Changed! :)\n");
+			file_buffer_before = file_buffer_after;
+		}
 		sleep(1);
 		system("clear");
 		i++;
 	}
 }
 int list_direcory(){
-	DIR *dp;
-	struct dirent *ep; 
-	int s = 0;
-	dp = opendir ("./");
+	DIR *dp; // For directory
+	struct dirent *ep; // Structure for listing directory
+	struct stat buffer; // Buffer for file information
+	int status, fildes; // File descriptor for file information
+	int s = 0; // File counter
+	dp = opendir ("./"); // Opening diretory
 	if (dp != NULL){
+		printf("\tFile name\tLast modified\n");
 		while ((ep = readdir (dp)) != NULL){
-			printf("File: %s\n", ep->d_name);
+			fildes = open(ep->d_name, O_RDWR); // Get file descriptor
+			status = fstat(fildes, &buffer); // Get file information
+			if(strcmp(ep->d_name, "..") && strcmp(ep->d_name, "."))
+				printf("\t%s \t %s", ep->d_name, ctime(&buffer.st_mtime));
 			s++;
 		}
 		(void) closedir (dp);
